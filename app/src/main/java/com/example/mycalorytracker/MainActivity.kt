f0fc142582e331eae909b86571ad281c8c80363f
Adrian Czerwinski <adrczer@gmail.com>
2022-10-10 06:16:05 +0200

    in proecess

--- app/src/main/java/com/example/mycalorytracker/MainActivity.kt
+++ app/src/main/java/com/example/mycalorytracker/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.mycalorytracker.onboarding_presentation.welcome.WelcomeScreen
 import com.example.mycalorytracker.ui.theme.MyCaloryTrackerTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,27 +18,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MyCaloryTrackerTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                WelcomeScreen()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    MyCaloryTrackerTheme {
-        Greeting("Android")
     }
 }
